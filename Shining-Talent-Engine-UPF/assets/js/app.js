912789cc4befee253770a595cda3116e46a569db
internetexplorer <meryemehdi@gmail.com>
2021-02-04 16:26:11 +0100

    working on the merg

---
+++ Shining-Talent-Engine-UPF/assets/js/app.js
@@ -0,0 +1,34 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+/*
+var navbar = document.querySelector(".navbar")
+var ham = document.querySelector(".ham")
+// Menu ...
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
+
+ham.addEventListener("click", toggleHamburger)
+
+// toggle when clicking on links
+
+// METHOD 1
+var menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach(
+  function(menuLink) {
+    menuLink.addEventListener("click", toggleHamburger)
+  }
+)
+*
